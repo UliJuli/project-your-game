02c654b776153999b3ee6399352111de0cedcea7
Дмитрий Кислицин <ctpaep@yandex.ru>
2022-09-06 21:14:40 +0800

    krasota

--- client/your-game/src/components/auth/Registration.jsx
+++ client/your-game/src/components/auth/Registration.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 
 
-export default function Registration({setNameHendler}) {
+export default function Registration({ setNameHendler }) {
   const [inputs, setInputs] = useState({ username: '', useremail: '', password: '', });
   const navigate = useNavigate()
 
@@ -31,36 +31,40 @@ export default function Registration({setNameHendler}) {
       return alert(data.message);
     } else if (response.status === 301) {
       console.log("datareg", data)
-     setNameHendler(data.name)
+      setNameHendler(data.name)
       const user = { id: data.id, name: data.name, email: data.email };
       localStorage.removeItem("name");
       localStorage.setItem('name', user.name);
       navigate('/');
     }
   }
-  
+
   return (
-    <div className="container">
-      <h2>Зарегистрируйтесь</h2>
-      <hr />
-      {/* <form onSubmit={handleSubmit} > */}
-      <form onSubmit={submitHandler}>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Name</label>
-          <input type="text" name="username" value={inputs.username} onChange={inputHandler} className="form-control" id="exampleInputName1" />
+    <div className="container-mb">
+      <div className='row mb-5'>
+        <div class="col">
         </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" name="useremail" value={inputs.useremail} onChange={inputHandler} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        <div class="col-8">
+          <form onSubmit={submitHandler} className="p-4 p-md-5 border rounded-3 bg-light">
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Name</label>
+              <input type="text" name="username" value={inputs.username} onChange={inputHandler} className="form-control" id="exampleInputName1" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+              <input type="email" name="useremail" value={inputs.useremail} onChange={inputHandler} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+              <input type="password" name="password" value={inputs.password} onChange={inputHandler} className="form-control" id="exampleInputPassword1" />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+          </form>
         </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" name="password" value={inputs.password} onChange={inputHandler} className="form-control" id="exampleInputPassword1" />
+        <div class="col">
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-      <hr />
+      </div>
     </div>
   )
 }
